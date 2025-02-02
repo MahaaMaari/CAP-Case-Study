7afaa655e838d47afdfc1183cf3921a293f76249
MahaaMaari <jinucjm@gmail.com>
2025-02-02 23:35:25 +0530

    test type issue is fixed

--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,6 @@
 import cds from '@sap/cds';
 import { Employee } from '#cds-models/employee/srv/EmployeeService/ManageEmployee';
+import { describe } from 'node:test';
 const {GET,POST,PATCH,axios,expect}=cds.test(__dirname+'/..');
 const EDIT=(url:string)=>{
     POST(url+'/ManageEmployee.draftEdit',{});
@@ -7,3 +8,9 @@ const EDIT=(url:string)=>{
 const SAVE=(url:string)=>{
     POST(url+'/ManageEmployee.draftActivate');
 }
+describe('test1',()=>{
+    it('Test1',async()=>{
+        let data=(await SELECT.from(Employee));
+        expect(data).to.exist;
+    })
+})
