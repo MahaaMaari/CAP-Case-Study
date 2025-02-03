378ff40798657b46da0da15de7f69322d687287c
Joy <jinu.joy01@sap.com>
2025-02-03 10:57:29 +0530

    testi issue finallu fixed

--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,5 @@
 import cds from '@sap/cds';
 import { Employee } from '#cds-models/employee/srv/EmployeeService/ManageEmployee';
-import { describe } from 'node:test';
 const {GET,POST,PATCH,axios,expect}=cds.test(__dirname+'/..');
 const EDIT=(url:string)=>{
     POST(url+'/ManageEmployee.draftEdit',{});
@@ -8,23 +7,9 @@ const EDIT=(url:string)=>{
 const SAVE=(url:string)=>{
     POST(url+'/ManageEmployee.draftActivate');
 }
-<<<<<<< HEAD
-axios.defaults.headers['content-type']='application/json';
-axios.defaults.auth={username:'admin',password:'admin@123'};
-
-describe('basic query',() => 
-    {
-        it('test',async()=>{
-            const data=SELECT.from(Employee);
-            expect(data).to.exist;
-        })
-     }
-)
-=======
 describe('test1',()=>{
     it('Test1',async()=>{
-        let data=(await SELECT.from(Employee));
+        let data=await SELECT.from(Employee);
         expect(data).to.exist;
     })
 })
->>>>>>> 7afaa655e838d47afdfc1183cf3921a293f76249
