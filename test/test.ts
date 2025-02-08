5f08ee9057b28fbd3dc7f08ad76549fbd6fd0e66
MahaaMaari <jinucjm@gmail.com>
2025-02-08 16:28:46 +0530

    test case

--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,7 @@
 import cds from '@sap/cds';
 const {GET,POST,PATCH,axios,expect}=cds.test(__dirname+'/..');
 import Emp from '../srv/EmployeeService/Employee';
+axios.defaults.auth={username:'admin',password:'admin@123'};
 const EDIT=(url:string)=>{
     POST(url+'/ManageEmployee.draftEdit',{});
 }
@@ -24,7 +25,29 @@ describe('Unit Tests for Employee',()=>{
             data:{empID:''}
         });
         await proxy.setEmployeeID(req);
-        expect(req.data.empID).exist;
+        expect(req.data.empID).to.exist;
         expect(req.data.empID).to.have.length(10);
+    })
+});
+
+describe('basic OData',()=>{
+    it('$metadata',async()=>{
+        const{headers,status}=await GET('/odata/v4/manage-employee/$metadata');
+        expect(status).to.equal(200);
+        expect(headers).to.contain(
+            {
+                'odata-version':'4.0'
+            }
+        );
+    });
+    it('GET Employee',async()=>{
+        const{data}=await GET('/odata/v4/manage-employee/Employee');
+        expect(data.value).to.be.an('array');
+    });
+    it('Create Employee',async()=>{
+        const {status,data}=await POST('/odata/v4/manage-employee/Employee',{
+            ID:cds.utils.uuid()
+        });
+        console.log(status);
     })
 })
